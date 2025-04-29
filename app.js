19e549307b36848eb1674899bf77099bcaa44f7e
Sopanha <137031850+Sopanha9@users.noreply.github.com>
2025-04-29 09:17:20 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function showBbb() {
+    document.getElementById("show_img").setAttribute("src", "/img/Bbb.jpg");
+}
+function showCutiePie() {
+    
+    document.getElementById("show_img").setAttribute("src", "/img/cutie pie.jpg");
+}
+function showPic() {
+    document.getElementById("show_img").setAttribute("src", "/img/pic.jpg");
+}
+
+function showSho() {
+    document.getElementById("show_img").setAttribute("src", "/img/sho.jpg");
+}
